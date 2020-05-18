b1a9b0462457359f570fb0afb0cad4994b725e90
Radical <romitradical@protonmail.com>
2020-05-18 22:54:16 +0530

    edited ui and added font

--- lib/src/animated_child.dart
+++ lib/src/animated_child.dart
@@ -14,21 +14,21 @@ class AnimatedChild extends AnimatedWidget {
   final Color titleColor;
   final Color subTitleColor;
 
-  AnimatedChild({
-    Key key,
-    Animation<double> animation,
-    this.index,
-    this.backgroundColor,
-    this.elevation = 6.0,
-    this.child,
-    this.title,
-    this.subtitle,
-    this.visible = false,
-    this.onTap,
-    this.toggleChildren,
-    this.titleColor,
-    this.subTitleColor
-  }) : super(key: key, listenable: animation);
+  AnimatedChild(
+      {Key key,
+      Animation<double> animation,
+      this.index,
+      this.backgroundColor,
+      this.elevation = 6.0,
+      this.child,
+      this.title,
+      this.subtitle,
+      this.visible = false,
+      this.onTap,
+      this.toggleChildren,
+      this.titleColor,
+      this.subTitleColor})
+      : super(key: key, listenable: animation);
 
   void _performAction() {
     if (onTap != null) onTap();
@@ -40,39 +40,49 @@ class AnimatedChild extends AnimatedWidget {
 
     final Widget buttonChild = animation.value > 50.0
         ? Container(
-          width: animation.value,
-          height: animation.value,
-          child: Row(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: child ?? Container(),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        title,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(color: (titleColor == null) ? Colors.black : titleColor, fontSize: 16.0),
-                      ),
-                      SizedBox(height: 3.0),
-                      Text(
-                        subtitle,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(color: (subTitleColor == null) ? Colors.black : subTitleColor, fontSize: 12.0),
-                      )
-                    ],
-                  ),
+            width: animation.value,
+            height: animation.value,
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: child ?? Container(),
                 ),
-              )
-            ],
-          ),
-        )
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          title,
+                          overflow: TextOverflow.clip,
+                          style: TextStyle(
+                              fontFamily: "Futura",
+                              color: (titleColor == null)
+                                  ? Colors.black
+                                  : titleColor,
+                              fontSize: 18.0),
+                        ),
+                        SizedBox(height: 4.0),
+                        Text(
+                          subtitle,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              fontFamily: "Futura",
+                              color: (subTitleColor == null)
+                                  ? Colors.black
+                                  : subTitleColor,
+                              fontSize: 12.0),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
         : Container(
             width: 0.0,
             height: 0.0,
@@ -80,14 +90,14 @@ class AnimatedChild extends AnimatedWidget {
 
     return Container(
       width: MediaQuery.of(context).size.width - 30,
-      height: 80.0,
-      padding: EdgeInsets.only(bottom: 72 - animation.value),
+      height: 100.0,
+      padding: EdgeInsets.only(bottom: 50 - animation.value),
       child: GestureDetector(
         onTap: _performAction,
         child: Card(
-          elevation: 2,
+          elevation: 5,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(7.0),
+            borderRadius: BorderRadius.circular(10),
           ),
           //width: animation.value,
           color: backgroundColor,
